dac370c4d85bcb0e7800844fe2fdabe1b41fde1f
ulltraviole <eekpublic@gmail.com>
2024-01-04 10:15:35 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace SayiTaminOyunu.Odev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int number;
+            string tahmin;
+            int tahminhakki;
+            int sayac = 0;
+            Console.WriteLine("Zorluk seçin.");
+            string zorluk = Console.ReadLine();
+            if (zorluk == "kolay")
+            {
+                number = rnd.Next(15);
+                tahminhakki = 10;
+            }
+            else if (zorluk == "zor")
+            {
+                number = rnd.Next(25);
+                tahminhakki = 3;
+            }
+            else
+            {
+                number = rnd.Next(20);
+                tahminhakki = 5;
+            }
+
+            do
+            {
+                sayac++;
+                tahminhakki--;
+                Console.WriteLine("Tahmin edin.");
+                tahmin = Console.ReadLine();
+            } while (Convert.ToInt32(tahmin) != number && tahminhakki > 0);
+            Console.WriteLine($"{sayac}. tahmininizde bildiniz.");
+            Console.WriteLine($"Puanınız: {100 - sayac * 10}");
+
+        }
+    }
+}
